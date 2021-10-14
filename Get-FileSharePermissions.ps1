da8687ccaf6e54cb890e6034c0241dff5aef2f9e
Kevin Lennan <klennan@gmail.com>
2021-10-14 14:01:49 -0500

    New file

---
+++ Get-FileSharePermissions.ps1
@@ -0,0 +1,27 @@
+function Get-FileSharePermissions {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position=0,Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [alias("Dir","FullName","FullPath")
+        [string]$Path,
+        [switch]$NoRecurse
+    )
+    Write-Verbose "Getting recursive directory list"
+    if (Test-Path $Path) {
+        if ($NoRecurse) {
+            $Path | Get-AclRule
+        }
+        else {
+            try {
+                Get-ChildItem -Path $Path -Recurse -Directory | Get-AclRule
+            }
+            catch {
+                Write-Error "Could not access $Path"
+                $Path | Get-AclRule
+            }
+        }
+    }
+    else {
+        Write-Error "Invalid path: $Path"
+    }
+}
